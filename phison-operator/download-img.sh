55b8a8d3a80d52d1a8530a3fd7d27b42e180e383
Wu Yi-Lin <terrywu25@gmail.com>
2026-01-19 16:33:17 +0800

    update phison operator

---
+++ phison-operator/download-img.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Phison Operator
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/aidaptivcache-operator:v0.7.3
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/aidaptivcache-node-discover:v0.1.1
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/aidaptivcache-device-plugin:v0.4.6
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/nvme-exporter:1.0.8
+
+## Ollama model
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/model:ollama-qwen3-06b
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/model:ollama-llama3-3-70b
