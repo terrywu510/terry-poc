ea9fb49bb997ca5b52ca5fd5d619aed2391887ef
Wu Yi-Lin <terrywu25@gmail.com>
2026-01-20 18:46:36 +0800

    update pg

--- helm-chart/postgresql/install.sh
+++ helm-chart/postgresql/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgresql:18.2.0
-sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/os-shell:18.2.0
-sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgres-exporter:18.2.0
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgresql:latest
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/os-shell:latest
+sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgres-exporter:latest
 
-sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgresql:18.2.0 registry-1.docker.io/bitnami/postgresql:18.2.0
-sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/os-shell:18.2.0 registry-1.docker.io/bitnami/os-shell:18.2.0
-sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgres-exporter:18.2.0 registry-1.docker.io/bitnami/postgres-exporter:18.2.0
+sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgresql:latest registry-1.docker.io/bitnami/postgresql:latest
+sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/os-shell:latest registry-1.docker.io/bitnami/os-shell:latest
+sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgres-exporter:latest registry-1.docker.io/bitnami/postgres-exporter:latest
