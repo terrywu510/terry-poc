68c3356d885abfbde311a264095468f98b0ef94a
Wu Yi-Lin <terrywu25@gmail.com>
2026-01-20 19:06:41 +0800

    update pg

--- helm-chart/postgresql/install.sh
+++ helm-chart/postgresql/install.sh
@@ -7,3 +7,5 @@ sudo ctr --namespace k8s.io image pull ghcr.io/terrywu510/registry-1.docker.io/b
 sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgresql:latest registry-1.docker.io/bitnami/postgresql:latest
 sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/os-shell:latest registry-1.docker.io/bitnami/os-shell:latest
 sudo ctr --namespace k8s.io image tag ghcr.io/terrywu510/registry-1.docker.io/bitnami/postgres-exporter:latest registry-1.docker.io/bitnami/postgres-exporter:latest
+
+helm install postgresql ./ --set global.defaultStorageClass="nfs-storage"
